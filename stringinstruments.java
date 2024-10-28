8a7a60a2dbdba648318dd395095627643b040a85
kinzaa <kinza_ahyan_33rpl@student.smktelkom-mlg.sch.id>
2024-10-28 10:57:41 +0700

    Add files via upload

---
+++ stringinstruments.java
@@ -0,0 +1,29 @@
+package music;
+
+public class stringinstruments extends instruments{
+  private int numString;
+
+  public int getNumString() {
+      return this.numString;
+  }
+
+  public void setNumString(int numString) {
+      this.numString = numString;
+  }
+
+  public stringinstruments () {
+      super("OnRight Guitar", 4000000);
+      numString = 1;
+  }
+
+  public stringinstruments (String name, double price, int numString) {
+      super();
+      this.numString = numString;
+  }
+
+  public void print() {
+      super.print();
+      System.out.println("Number : "+numString);
+  }
+
+}
